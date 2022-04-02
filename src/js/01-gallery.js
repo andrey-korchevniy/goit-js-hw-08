7bae10a3cfb1c7ee9c8f8f49bc16716f454d4d55
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-04-02 09:22:48 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`;
+         
+    })  
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    widthRatio: 0.95,
+    captionDelay: 500
+});
+
+
+gallery.on('show.simplelightbox');
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import playerVimeo from "@vimeo/player";
+
+const iframe = document.querySelector('iframe');
+const player = new playerVimeo(iframe);
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+player.on('timeupdate', function (event) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(event.seconds));
+});
 
